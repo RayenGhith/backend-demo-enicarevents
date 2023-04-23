059b52b8fbdde9cbd3f3dac1a2381c8e19f6557a
RayenGhith <rghith@yahoo.com>
2023-04-23 20:43:19 +0200

    Backend Commit

--- src/main/java/web/demo/repo/SubscriberRepository.java
+++
@@ -1,9 +0,0 @@
-package web.demo.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import web.demo.model.Subscriber;
-
-@Repository
-public interface SubscriberRepository extends CrudRepository<Subscriber, String> {
-}
